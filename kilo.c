e2fede0d2be0a715e5e76885db6ec069904d5812
pjkpjk900 <pk58@buffalo.edu>
2020-03-15 02:12:25 -0400

    Step 5

--- kilo.c
+++ kilo.c
@@ -1,3 +1,12 @@
+#include<unistd.h>
+
 int main(){
+    char c;
+    /*
+    To find more information about read, run: man 2 read
+    read returns the number of bytes read on success and -1 on failure.
+    STDIN_FILENO is a macro defined in <unistd.h> that is a file descriptor for stdin, which is always 0.
+    */
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
